b6177e5b63f5b164e7cbb9311e5927aa7ebf90d6
Go Market <sonupvlog8937@gmail.com>
2024-08-01 10:55:22 +0530

    first commit

---
+++ frontend/src/pages/ForgotPasswordPage.js
@@ -0,0 +1,7 @@
+import ForgotPassword from "../features/auth/components/ForgotPassword";
+function ForgotPasswordPage() {
+    return ( <div>
+        <ForgotPassword></ForgotPassword>
+    </div>  );
+}
+export default ForgotPasswordPage;
